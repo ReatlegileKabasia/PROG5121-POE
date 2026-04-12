9deda70deeaf737e8ad2cd8bc81ff8b7491ff7bd
ReatlegileKabasia <ST10534314@rcconnect.edu.za>
2026-04-12 17:37:57 +0200

    Add files via upload
    Initial project setup and Login class variables

---
+++ Login.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Login {
+    // Fields to store user information [cite: 108, 126]
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String cellNumber;
+}
